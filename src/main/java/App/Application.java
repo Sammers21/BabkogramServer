961145e144cf501de6971cd8e05f1aaa7a34d741
Sammers21 <titantins@ya.ru>
2016-09-22 16:01:33 +0300

    change location

---
+++ src/main/java/App/Application.java
@@ -0,0 +1,14 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("App.controllers")
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

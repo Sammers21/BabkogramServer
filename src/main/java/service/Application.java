da229fdf3b9b709768c2107f9bb3291ccfafd2f1
Sammers21 <titantins@ya.ru>
2016-10-31 00:11:18 +0300

    registry service has been fixed

--- src/main/java/service/Application.java
+++ src/main/java/service/Application.java
@@ -19,47 +19,12 @@ public class Application {
 
     private static final Logger log = Logger.getLogger(Application.class);
 
-    @Autowired
-    UserRepository userRepository;
-
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
-    //@Bean
-    /*CommandLineRunner demo(UserRepository repository) {
-        return (args) -> {
-            // save a couple of customers
-            repository.save(new User("Jack", "Bauer"));
-            repository.save(new User("Chloe", "O'Brian"));
-            repository.save(new User("Kim", "Bauer"));
-            repository.save(new User("David", "Palmer"));
-            repository.save(new User("Michelle", "Dessler"));
 
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            for (User customer : repository.findAll()) {
-                log.info(customer.toString());
-            }
-            log.info("");
-
-            // fetch an individual customer by ID
-            User customer = repository.findOne(1L);
-            log.info("Customer found with findOne(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            for (User bauer : repository.findByUsername("Bauer")) {
-                log.info(bauer.toString());
-            }
-            log.info("");
-        };
-    }*/
 
 
 }
--- src/main/java/service/objects/Token.java
+++ src/main/java/service/objects/Token.java
@@ -1,5 +1,9 @@
 package service.objects;
 
+import service.repository.UserRepository;
+
+import java.util.Random;
+
 public class Token {
 
     private String token;
@@ -21,5 +25,24 @@ public class Token {
     public Token(String token) {
 
         this.token = token;
+    }
+
+    private static final Random rnd = new Random();
+
+    public static Token generate(UserRepository userRepository) {
+        StringBuilder stringBuilder;
+
+        do {
+            stringBuilder = new StringBuilder("");
+            String pattern = "ABCDEFGHIKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+
+            for (int i = 0; i < 32; i++) {
+                stringBuilder.append(pattern.charAt(rnd.nextInt(pattern.length())));
+            }
+
+        } while (userRepository.findByToken(stringBuilder.toString()) != null);
+
+        return new Token(stringBuilder.toString());
+
     }
 }

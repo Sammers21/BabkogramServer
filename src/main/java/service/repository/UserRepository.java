33e3a6f2b8469ce89bff885b32ed83e7a5c9ccd0
Sammers21 <titantins@ya.ru>
2016-10-31 00:07:08 +0300

    fixed returned typer Repository queries

--- src/main/java/service/repository/UserRepository.java
+++ src/main/java/service/repository/UserRepository.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 
+
+    User findByToken(String token);
 }
